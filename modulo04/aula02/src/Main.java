b1bade416e839ab59b17a94f04318fdcc4735e58
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-02-26 19:04:58 -0300

    Trabalhando com processamento de dados e casting

---
+++ modulo04/aula02/src/Main.java
@@ -0,0 +1,57 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // Exemplo 01
+        //int x, y;
+        //int x;
+        //double y;
+
+        //x = 5;
+        //y = 2 * x;
+
+        //System.out.println(x);
+        //System.out.println(y);
+
+        // Exemplo 02
+        //double b, B, h, area;
+
+        //b = 6.0;
+        //B = 8.0;
+        //h = 5.0;
+
+        //area = (b + B) / 2.0 * h;
+
+        //System.out.println(area);
+
+        // Exemplo 03
+        // int a, b;
+        // double resultado;
+
+        // a = 5;
+        // b = 2;
+
+        // resultado = a / b;
+
+        // System.out.println(resultado);
+
+        // Exemplo 04 utilizando casting
+        // int a, b;
+        // double resultado;
+
+        // a = 5;
+        // b = 2;
+
+        // resultado = (double) a / b;
+
+        // System.out.println(resultado);
+
+        // Exemplo 05 utilizando casting
+        double a;
+        int b;
+
+        a = 5.0;
+        b = (int) a;
+
+        System.out.println(b);
+    }
+}
