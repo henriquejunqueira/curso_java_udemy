2a61c25b8094c7b0d411c560bb3d8f9e0bc4964b
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-08-06 22:47:20 -0300

    Exercício 02

--- modulo04/Exercicios03-estrutura-while/src/Main.java
+++ modulo04/Exercicios03-estrutura-while/src/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
+
         // Exercício 01
         /*
         * Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
@@ -10,20 +12,42 @@ public class Main {
         * impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
         */
 
-        Scanner sc = new Scanner(System.in);
+//        int senha = 2002;
+//
+//        int senha_digitada = sc.nextInt();
+//
+//        while (senha_digitada != senha){
+//            System.out.println("Senha Invalida");
+//            senha_digitada = sc.nextInt();
+//        }
+//
+//        System.out.println("Acesso Permitido");
 
-        int senha = 2002;
+        // Exercício 02
+        /*
+        * Escreva um programa para ler as coordenadas (X,Y) de uma quantidade indeterminada de pontos no sistema
+        * cartesiano. Para cada ponto escrever o quadrante a que ele pertence. O algoritmo será encerrado quando pelo
+        * menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem alguma).
+        */
 
-        int senha_digitada = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
 
-        while (senha_digitada != senha){
-            System.out.println("Senha Invalida");
-            senha_digitada = sc.nextInt();
+        while(x != 0 && y != 0){
+
+            if(x > 0 && y > 0){
+                System.out.println("primeiro");
+            }else if(x < 0 && y > 0){
+                System.out.println("segundo");
+            }else if(x < 0 && y < 0){
+                System.out.println("terceiro");
+            } else if (x > 0 && y < 0) {
+                System.out.println("quarto");
+            }
+
+            x = sc.nextInt();
+            y = sc.nextInt();
         }
-
-        System.out.println("Acesso Permitido");
-
-        sc.close();
 
     }
 }
