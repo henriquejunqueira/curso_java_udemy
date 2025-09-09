61a8294287bbc3f2000bf2b0f119cf16af086d0c
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-09-08 21:30:26 -0300

    Gerando automaticamente construtores, getters e setters

--- modulo07/Aula03/src/entities/Product.java
+++ modulo07/Aula03/src/entities/Product.java
@@ -20,23 +20,23 @@ public class Product {
         this.price = price;
     }
 
-    public  String getName(){
+    public String getName() {
         return name;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public double getPrice(){
+    public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price){
+    public void setPrice(double price) {
         this.price = price;
     }
 
-    public int getQuantity(){
+    public int getQuantity() {
         return quantity;
     }
 
